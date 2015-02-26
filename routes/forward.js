0343597b2c0e2ea249e5cfa89eadf8a87ceeb55c
ark930 <wangxiao_8800@qq.com>
2015-02-26 10:32:10 +0800

    update forward.js

--- routes/forward.js
+++ routes/forward.js
@@ -4,11 +4,15 @@ var router = express.Router();
 var net = require('net');
 var HOST = '0.0.0.0';
 var PORT = 8880;
-var socket;
+var sockets = [];
 
 router.post('/', function(req, res, next) {
-    if (socket) {
-        socket.write(JSON.stringify(req.body));
+    if (sockets) {
+        for(var i = 0; i < sockets.length; i++)
+        {
+            if(sockets[i])
+                sockets[i].write(JSON.stringify(req.body));
+        }
         res.send(req.body);
     } else {
         res.status(400).send('Socket not connected');
@@ -19,7 +23,19 @@ var server = net.createServer();
 server.listen(PORT, HOST);
 server.on('connection', function(sock) {
     console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-    socket = sock;
+    sockets.push(sock);
+    //sock.write('123');
 });
+
+//server.on('close', function(sock) {
+//    console.log('CLOSE: ' + sock.remoteAddress +':'+ sock.remotePort);
+//    for(var i = 0; i < sockets.length; i++)
+//    {
+//        if(sockets[i] == sock)
+//        {
+//            sockets[i].splice(i, 1);
+//        }
+//    }
+//});
 
 module.exports = router;
